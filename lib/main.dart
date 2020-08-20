d763c9480e840a25e8bf01a3f5c03b485058845d
jsaintz <jonatas_santos3161@hotmail.com>
2020-08-20 01:28:58 -0300

    text style in column.

--- lib/main.dart
+++ lib/main.dart
@@ -36,8 +36,45 @@ class MyHomePage extends StatelessWidget {
             ),
           ),
           Column(
-            children: _trasactions.map((tr){
-              return Card(child: Text(tr.title),);
+            children: _trasactions.map((tr) {
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.purple, width: 2),
+                      ),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        tr.value.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          tr.title,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          tr.date.toString(),
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
             }).toList(),
           )
         ],
